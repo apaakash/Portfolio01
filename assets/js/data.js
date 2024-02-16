f6a497338c17696a7ec513847184990f958c1981
apaakash <144619858+apaakash@users.noreply.github.com>
2024-02-16 18:47:42 +0530

    Add files via upload
    portfolio

---
+++ assets/js/data.js
@@ -0,0 +1,16 @@
+function SendData()
+{
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "3idiotsvlogs0@gmail.com",
+        Password : "9C40169564AE1D2A899CB214826F41F94AE1",
+        To : '3idiotsvlogs0@gmail.com',
+        From : document.getElementById("E1").value,
+        Subject : "Portfolio Mail",
+        Body : "Name: " + document.getElementById("name1").value
+            + "<br> Email: " + document.getElementById("E2").value
+            + "<br> Message: " + document.getElementById("message").value
+    }).then(
+      message => alert("Data sent successfully")
+    );
+}
